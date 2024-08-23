1ecfba88d0c228cd576e220078f250896c8819c7
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-08-24 04:18:25 +1000

    Added active status

--- Element.cs
+++ Element.cs
@@ -8,6 +8,27 @@ public class Element
     public string ObjectId { get; } = Guid.NewGuid().ToString();
     public string Name;
 
+    // == Element Status == //
+    private bool _isActive = true;
+    public bool IsActive
+    {
+        get => _isActive;
+        set
+        {
+            if(value != _isActive)
+            {
+                _isActive = value;
+                if (_isActive)
+                    Enable?.Invoke();
+                else
+                    Disable?.Invoke();
+            }
+        }
+    }
+
+    public System.Action Enable;
+    public System.Action Disable;
+
     public Element(string name = "Element")
     {
         this.Name = name;
