65795bb334c32fa0a6e246853572615eacdceaf3
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-08-24 04:16:21 +1000

    Added Element class

---
+++ Element.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Raylib_cs;
+
+namespace Vortex;
+
+public class Element
+{
+    public string ObjectId { get; } = Guid.NewGuid().ToString();
+    public string Name;
+
+    public Element(string name = "Element")
+    {
+        this.Name = name;
+    }
+
+    public virtual void Initialize()
+    {
+
+    }
+
+    public virtual void Start()
+    {
+
+    }
+
+    public virtual void Update(float dt)
+    {
+
+    }
+
+    public virtual void Draw()
+    {
+
+    }
+
+    public virtual void Destroy()
+    {
+
+    }
+}
