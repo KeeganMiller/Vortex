c8ed155aeba33a755c047947b5cf9faf45b76dbf
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-08-24 14:25:25 +1000

    Created the default component

---
+++ Components/Component.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Raylib_cs;
+
+namespace Vortex;
+
+public class Component
+{
+    public string ComponentId { get; } = Guid.NewGuid().ToString();
+    public string Name;
+    public Element Owner { get; private set; }
+
+    public Component(string name = "Component")
+    {
+        Name = name;
+    }
+
+    public virtual void Initialize(Element owner)
+    {
+        Owner = owner;
+    }
+
+    public virtual void Start()
+    {
+
+    }
+
+    public virtual void Update(float dt)
+    {
+
+    }
+
+    public virtual void Draw()
+    {
+
+    }
+
+    public virtual void Destroy()
+    {
+
+    }
+}
