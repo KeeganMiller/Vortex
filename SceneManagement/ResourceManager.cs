c94ed9c002ff294983527f34fe50c205dca47328
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-08-24 14:52:36 +1000

    Resource manageR

--- SceneManagement/ResourceManager.cs
+++ SceneManagement/ResourceManager.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Raylib_cs;
 
-namespace Vortex.SceneManagement
+namespace Vortex;
+
+public class ResourceManager
 {
-    internal class ResourceManager
+    public Scene Owner { get; }
+    public ResourceManager(Scene owner)
     {
+        Owner = owner;
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Update(float dt)
+    {
+
+    }
+
+    public void Draw()
+    {
+
+    }
+
+    public void Stop()
+    {
+
     }
 }
