1fa98110a0820c7f9315df8afc065533566b353d
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-08-24 14:56:58 +1000

    Added method to add and remove from resource manager

--- SceneManagement/ResourceManager.cs
+++ SceneManagement/ResourceManager.cs
@@ -6,6 +6,10 @@ namespace Vortex;
 public class ResourceManager
 {
     public Scene Owner { get; }
+
+    private List<Element> _elements = new List<Element>();
+    private bool _hasElementToStart = false;
+
     public ResourceManager(Scene owner)
     {
         Owner = owner;
@@ -29,5 +33,29 @@ public class ResourceManager
     public void Stop()
     {
 
+    }
+
+    public bool AddElement(Element element)
+    {
+        if(element != null && !_elements.Contains(element)) 
+        {
+            _elements.Add(element);
+            element.Initialize(this);
+            _hasElementToStart = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool RemoveElement(Element element)
+    {
+        if(element != null && _elements.Contains(element))
+        {
+            _elements.Remove(element);
+            return true;
+        }
+
+        return false;
     }
 }
