a207a89d3794e861deb65f5aa72e26972b82b08d
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-08-24 14:50:19 +1000

    Setup the scene class

---
+++ SceneManagement/Scene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Raylib_cs;
+
+namespace Vortex;
+
+public abstract class Scene
+{
+    public string SceneId { get; } = Guid.NewGuid().ToString();
+    public string SceneName;
+    protected string _sceneDataPath { get; private set; }
+
+    public Scene(string name, string scenePath)
+    {
+        SceneName = name;
+        _sceneDataPath = scenePath;
+    }
+
+    public virtual void Enter()
+    {
+
+    }
+
+    public virtual void Exit()
+    {
+
+    }
+
+    public virtual void Update(float dt)
+    {
+
+    }
+
+    public virtual void Draw()
+    {
+
+    }
+}
