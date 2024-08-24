f3fbfcb14f91c7c1e9483b784c65a2196a992580
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-08-24 14:54:06 +1000

    Called event methods on resource manager

--- SceneManagement/Scene.cs
+++ SceneManagement/Scene.cs
@@ -9,30 +9,36 @@ public abstract class Scene
     public string SceneId { get; } = Guid.NewGuid().ToString();
     public string SceneName;
     protected string _sceneDataPath { get; private set; }
+    public ResourceManager Resources { get; }
 
     public Scene(string name, string scenePath)
     {
         SceneName = name;
         _sceneDataPath = scenePath;
+        Resources = new ResourceManager(this);
     }
 
     public virtual void Enter()
     {
-
+        if (Resources != null)
+            Resources.Start();
     }
 
     public virtual void Exit()
     {
-
+        if (Resources != null)
+            Resources.Stop();
     }
 
     public virtual void Update(float dt)
     {
-
+        if (Resources != null)
+            Resources.Update(dt);
     }
 
     public virtual void Draw()
     {
-
+        if (Resources != null)
+            Resources.Draw();
     }
 }
