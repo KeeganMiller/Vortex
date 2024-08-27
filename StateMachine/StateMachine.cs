1bf1fdb0353137915058f5e13c298a3573e37450
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-08-27 15:26:32 +1000

    Added properties

--- StateMachine/StateMachine.cs
+++ StateMachine/StateMachine.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Vortex;
 
@@ -6,6 +7,8 @@ public abstract class StateMachine : Component
 {
     protected State _currentState;                      // Reference to the state we are currently updating
     public State EntryState;                            // State that will run when the state machine starts
+
+    public List<StateProperty> Properties { get; private set;} = new List<StateProperty>();
 
     public override void Start()
     {
@@ -51,5 +54,43 @@ public abstract class StateMachine : Component
         _currentState = state;
         if(_currentState != null)
             _currentState.Enter();
+    }
+
+    /// <summary>
+    /// Sets/Creates Property value
+    /// </summary>
+    /// <typeparam name="T">Type of value</typeparam>
+    /// <param name="key">Key for the property</param>
+    /// <param name="value">New value of the property</param>
+    /// <param name="propType">The type of property that is stored (only for new Properties)</param>
+    public void SetPropertyValue<T>(string key, T value, EStatePropertyType propType = EStatePropertyType.PROP_None)
+    {
+        foreach(var prop in Properties)
+        {
+            if(prop.Key == key && prop is StateValue<T> propVal)
+            {
+                propVal.Value = value;
+            }
+        }
+
+        Properties.Add(new StateValue<T>(key, value, propType));
+    }
+
+    /// <summary>
+    /// Gets the value of a property
+    /// </summary>
+    /// <typeparam name="T">Type of property value</typeparam>
+    /// <param name="key">Key for the property to find</param>
+    /// <returns>Value of the state property</returns>
+    public T GetPropertyValue<T>(string key)
+    {
+        foreach(var prop in Properties)
+        {
+            if(prop.Key == key && prop is StateValue<T> propVal)
+                return propVal.Value;
+        }
+        
+        Debug.Print($"StateMachine::GetPropertyValue -> Failed to find property: {key}", EPrintMessageType.PRINT_Warning);
+        return default;
     }
 }
