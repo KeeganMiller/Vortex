9cebc67a7f95d79b91a6077e1a6080007d92e73d
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-03 15:12:21 +1000

    Added sizing of the text component

--- Components/UI/TextComponent.cs
+++ Components/UI/TextComponent.cs
@@ -9,14 +9,40 @@ public class TextComponent : UIComponent
     public Font NormalFont;
     public Shader FontShader;
 
-
-    public int FontSize = 16;
-    public string Text = "";
+    private int _fontSize = 16;
+    public int FontSize
+    {
+        get => _fontSize;
+        set 
+        {
+            _fontSize = value;
+            var componentSize = Raylib.MeasureTextEx(NormalFont, value, FontSize, 1);
+            Width = componentSize.X;
+            Height = componentSize.Y;
+        }
+    }
+    private string _text = "";
+    public string Text 
+    {
+        get => _text;
+        set 
+        {
+            _text = value;
+            var componentSize = Raylib.MeasureTextEx(NormalFont, value, FontSize, 1);
+            Width = componentSize.X;
+            Height = componentSize.Y;
+        }
+    }
     public Color FontColor = Color.Black;
 
     public override void Start()
     {
         base.Start();
+    }
+
+    public override void Update(float dt)
+    {
+        base.Update(dt);
     }
 
     public override void Draw()
