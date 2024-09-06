772ddadb7997e46acaf3d66a8c314c2a277b25e5
KeeganMiller <126998795+KeeganMiller@users.noreply.github.com>
2024-09-06 13:30:40 +1000

    Added update properties event

--- Components/TransformComponent.cs
+++ Components/TransformComponent.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
 using Raylib_cs;
 
@@ -11,20 +12,38 @@ public class TransformComponent : Component
     public Vector2 Position
     {
         get => Owner.Parent != null ? Owner.Parent.Transform.Position + _localPosition : _localPosition;
-        set => _localPosition = value;
+        set 
+        {
+            _localPosition = value;
+            PositionUpdateEvent?.Invoke();
+        }
     }
+    
+    public Action? PositionUpdateEvent;
 
     private Vector2 _localScale = Vector2.One;
     public Vector2 Scale
     {
         get => Owner.Parent != null ? Owner.Parent.Transform.Scale * _localScale : _localScale;
-        set => _localScale = value;
+        set 
+        {
+            _localScale = value;
+            ScaleUpdateEvent?.Invoke();
+        }
     }
+
+    public Action? ScaleUpdateEvent;
 
     private float _localRotation = 0;
     public float Rotation
     {
         get => Owner.Parent != null ? Owner.Parent.Transform.Rotation + _localRotation : _localRotation;
-        set => _localRotation = value;
+        set 
+        {
+            _localRotation = value;
+            RotationUpdateEvent?.Invoke();
+        }
     }
+
+    public Action? RotationUpdateEvent;
 }
